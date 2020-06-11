386c3186ce9dc3144fdb32bebfdf485c0ac73247
Andrei Catalin Negura <c16733435@mydit.ie>
2020-06-11 22:25:01 +0100

    Removed Forms project

--- Forms/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {AppComponent} from "./app.component";
-
-
-export const rootRouterConfig: Routes = [
-  //{ path: '', component: AppComponent },
-];
-
